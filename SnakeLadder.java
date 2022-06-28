317abf6d5c1d34feaba205691c81905c453cda6c
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 14:45:17 +0530

    Added : Dice Number Program

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,15 @@
-
+import java.util.Random;
 
 public class SnakeLadder {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game");
-
+        int diceNum = getDieRoll();
     }
+        private static int getDieRoll() {
+            Random random = new Random();
+            int roll = random.nextInt(6) + 1; // (max - min + 1) + min
+            System.out.println(roll);
+            return roll;
+        }
 }
