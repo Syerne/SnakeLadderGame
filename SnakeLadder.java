786738b415771dbbc3122467ba5fddb0ae48873c
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 14:38:50 +0530

    Added : Player Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,5 +5,9 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game");
 
+        int player =0;
+
+        System.out.println("Player Start Position " + player);
+
     }
 }
